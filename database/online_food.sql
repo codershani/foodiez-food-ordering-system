432a5a732a0d73941ea333595aae04be2d092a13
codershani <codershani13@gmail.com>
2024-02-16 19:00:03 +0530

    Made multiple changes on update order page and changes on homepage

--- database/online_food.sql
+++ database/online_food.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 23, 2020 at 07:28 AM
--- Server version: 10.1.28-MariaDB
--- PHP Version: 7.0.25
+-- Generation Time: Jan 30, 2024 at 05:55 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.1.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -34,7 +33,7 @@ CREATE TABLE `admin` (
   `username` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `admin`
@@ -59,7 +58,7 @@ CREATE TABLE `banner` (
   `order_number` int(11) NOT NULL,
   `added_on` datetime NOT NULL,
   `status` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `banner`
@@ -67,7 +66,7 @@ CREATE TABLE `banner` (
 
 INSERT INTO `banner` (`id`, `image`, `heading`, `sub_heading`, `link`, `link_txt`, `order_number`, `added_on`, `status`) VALUES
 (1, '533799913_banner-4.jpg', 'Drink & Heathy Food', 'Fresh Heathy and Organic', 'shop', 'Order Now', 1, '2020-06-23 03:00:05', 1),
-(2, '546847873_banner-4.jpg', 'Drink & Heathy Food', 'Fresh Heathy and Organic', 'shop', 'Order Now', 1, '2020-06-23 03:06:53', 1);
+(2, '643736091_hero-bg.jpg', 'Fast Food Restaurant', 'Eat Fresh, Feel Fresh, Fresh Out of Excuses!', 'shop', 'Order Now', 1, '2020-06-23 03:06:53', 1);
 
 -- --------------------------------------------------------
 
@@ -81,7 +80,7 @@ CREATE TABLE `category` (
   `order_number` int(11) NOT NULL,
   `status` int(11) NOT NULL,
   `added_on` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `category`
@@ -92,7 +91,7 @@ INSERT INTO `category` (`id`, `category`, `order_number`, `status`, `added_on`) 
 (2, 'Chinese', 2, 1, '2020-06-16 12:06:41'),
 (3, 'South Indian', 3, 1, '2020-06-16 12:06:59'),
 (4, 'Desserts', 4, 1, '2020-06-16 12:07:18'),
-(5, 'Murg', 0, 1, '2020-06-27 12:49:37');
+(6, 'Fast Foods', 5, 1, '2024-01-30 09:26:02');
 
 -- --------------------------------------------------------
 
@@ -108,14 +107,7 @@ CREATE TABLE `contact_us` (
   `subject` varchar(200) NOT NULL,
   `message` text NOT NULL,
   `added_on` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `contact_us`
---
-
-INSERT INTO `contact_us` (`id`, `name`, `email`, `mobile`, `subject`, `message`, `added_on`) VALUES
-(1, 'Vishal', 'phpvishal@gmail.com', '9999999999', 'Test Subject', 'test message', '2020-06-23 03:21:43');
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
 
@@ -132,7 +124,7 @@ CREATE TABLE `coupon_code` (
   `expired_on` date NOT NULL,
   `status` int(11) NOT NULL,
   `added_on` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `coupon_code`
@@ -155,15 +147,15 @@ CREATE TABLE `delivery_boy` (
   `password` varchar(50) NOT NULL,
   `status` int(11) NOT NULL,
   `added_on` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `delivery_boy`
 --
 
 INSERT INTO `delivery_boy` (`id`, `name`, `mobile`, `password`, `status`, `added_on`) VALUES
-(1, 'Vishal', '1234567890', 'vishal', 1, '2020-07-08 08:06:06'),
-(2, 'Amit', '1234567891', 'amit', 1, '2020-07-08 08:06:21');
+(1, 'Priyanshu', '1234567890', 'priyanshu', 1, '2020-07-08 08:06:06'),
+(2, 'Shani', '1234567891', 'shani', 1, '2020-07-08 08:06:21');
 
 -- --------------------------------------------------------
 
@@ -180,7 +172,7 @@ CREATE TABLE `dish` (
   `type` enum('veg','non-veg') NOT NULL,
   `status` int(11) NOT NULL,
   `added_on` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `dish`
@@ -204,14 +196,16 @@ CREATE TABLE `dish_cart` (
   `dish_detail_id` int(11) NOT NULL,
   `qty` int(11) NOT NULL,
   `added_on` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `dish_cart`
 --
 
 INSERT INTO `dish_cart` (`id`, `user_id`, `dish_detail_id`, `qty`, `added_on`) VALUES
-(3, 2, 6, 2, '2020-07-21 09:18:31');
+(3, 2, 6, 2, '2020-07-21 09:18:31'),
+(7, 6, 6, 1, '2024-01-28 05:44:17'),
+(12, 7, 2, 1, '2024-01-30 09:14:58');
 
 -- --------------------------------------------------------
 
@@ -226,7 +220,7 @@ CREATE TABLE `dish_details` (
   `price` int(11) NOT NULL,
   `status` int(11) NOT NULL,
   `added_on` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `dish_details`
@@ -253,7 +247,7 @@ CREATE TABLE `order_detail` (
   `dish_details_id` int(11) NOT NULL,
   `price` float NOT NULL,
   `qty` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `order_detail`
@@ -262,7 +256,9 @@ CREATE TABLE `order_detail` (
 INSERT INTO `order_detail` (`id`, `order_id`, `dish_details_id`, `price`, `qty`) VALUES
 (1, 1, 6, 40, 6),
 (2, 2, 6, 40, 4),
-(3, 3, 6, 40, 3);
+(3, 3, 6, 40, 3),
+(4, 4, 2, 170, 1),
+(5, 5, 2, 170, 1);
 
 -- --------------------------------------------------------
 
@@ -290,16 +286,7 @@ CREATE TABLE `order_master` (
   `cancel_at` datetime NOT NULL,
   `added_on` datetime NOT NULL,
   `delivered_on` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `order_master`
---
-
-INSERT INTO `order_master` (`id`, `user_id`, `name`, `email`, `mobile`, `address`, `total_price`, `coupon_code`, `final_price`, `zipcode`, `delivery_boy_id`, `payment_status`, `payment_type`, `payment_id`, `order_status`, `cancel_by`, `cancel_at`, `added_on`, `delivered_on`) VALUES
-(1, 2, 'Vishal', 'ervishalwebdeveloper@gmail.com', '9999999999', 'Test', 240, '', 240, '110076', 0, 'pending', 'wallet', '', 5, 'admin', '2020-07-21 08:13:01', '2020-07-18 06:08:19', '0000-00-00 00:00:00'),
-(2, 2, 'Vishal', 'ervishalwebdeveloper@gmail.com', '9999999999', 'test', 160, '', 160, '110076', 0, 'pending', 'wallet', '', 4, 'user', '0000-00-00 00:00:00', '2020-07-20 06:09:59', '0000-00-00 00:00:00'),
-(3, 5, 'Vishal', 'phpvishal@gmail.com', '9999999999', 'Test', 120, '', 120, '110076', 0, 'pending', 'cod', '', 4, 'user', '0000-00-00 00:00:00', '2020-07-23 09:09:41', '0000-00-00 00:00:00');
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
 
@@ -310,7 +297,7 @@ INSERT INTO `order_master` (`id`, `user_id`, `name`, `email`, `mobile`, `address
 CREATE TABLE `order_status` (
   `id` int(11) NOT NULL,
   `order_status` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `order_status`
@@ -335,7 +322,7 @@ CREATE TABLE `rating` (
   `order_id` int(11) NOT NULL,
   `dish_detail_id` int(11) NOT NULL,
   `rating` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `rating`
@@ -358,7 +345,7 @@ CREATE TABLE `setting` (
   `wallet_amt` int(11) NOT NULL,
   `website_close_msg` varchar(250) NOT NULL,
   `referral_amt` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `setting`
@@ -385,15 +372,14 @@ CREATE TABLE `user` (
   `referral_code` varchar(20) NOT NULL,
   `from_referral_code` varchar(20) NOT NULL,
   `added_on` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`id`, `name`, `email`, `mobile`, `password`, `status`, `email_verify`, `rand_str`, `referral_code`, `from_referral_code`, `added_on`) VALUES
-(2, 'Vishal', 'ervishalwebdeveloper@gmail.com', '9999999999', '$2y$10$cZ6sSDVYUINCnynmB3Tbuend7e7bRqgCPeD.RPmxdRucxKhbSo/52', 1, 1, 'lmkaetixyrzqoqy', 'lmkaetixyrzqoqds', '', '2020-07-18 05:11:37'),
-(5, 'Vishal', 'phpvishal@gmail.com', '9999999999', '$2y$10$wWBWMJTK37jkyLujN8YQoOVfQQt/3.B9ruoaNkjruQEwz4ZVfvNJa', 1, 1, 'zdoxxhajyeqtgpp', 'lhqtkjofzevfdai', 'lmkaetixyrzqoqds', '2020-07-23 08:48:08');
+(7, 'Shani Sharma', 'sarmashani933@gmail.com', '9999999999', '$2y$10$CtzeNO9O9MOavFgincmLnu/3UvjsSsi2Si.W1yMsiGnMVYPtU/JwG', 1, 1, 'ycjrofhumlunqsj', 'sracgwozdjbxwus', '', '2024-01-28 06:09:46');
 
 -- --------------------------------------------------------
 
@@ -409,29 +395,14 @@ CREATE TABLE `wallet` (
   `type` enum('in','out') NOT NULL,
   `payment_id` varchar(50) NOT NULL,
   `added_on` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `wallet`
 --
 
 INSERT INTO `wallet` (`id`, `user_id`, `amt`, `msg`, `type`, `payment_id`, `added_on`) VALUES
-(2, 2, 50, 'Registration', 'in', '', '2020-07-18 05:11:38'),
-(4, 2, 50, 'Shoping', 'out', '', '0000-00-00 00:00:00'),
-(5, 2, 100, 'Added', 'in', '', '0000-00-00 00:00:00'),
-(7, 2, 100, 'Added', 'in', '', '2020-07-18 05:58:29'),
-(8, 2, 20, 'Added', 'in', '', '2020-07-18 05:59:02'),
-(9, 2, 15, 'Added', 'in', '', '2020-07-18 06:00:35'),
-(10, 2, 30, 'Added', 'in', '', '2020-07-18 06:01:17'),
-(11, 2, 10, 'Added', 'in', '20200718111212800110168602301710786', '2020-07-18 06:04:04'),
-(13, 2, 160, 'Order Id-2', 'out', '', '2020-07-18 06:09:59'),
-(14, 2, 800, 'Added', 'in', '20200718111212800110168644701732407', '2020-07-18 06:17:19'),
-(15, 3, 0, 'Register', 'in', '', '2020-07-18 08:00:53'),
-(16, 2, 200, 'Order Id-3', 'out', '', '2020-07-19 04:29:04'),
-(17, 2, 200, 'Order Id-4', 'out', '', '2020-07-19 04:30:51'),
-(18, 3, 100, 'Test msg', 'in', '', '2020-07-21 08:22:33'),
-(19, 2, 200, 'Test Msg', 'in', '', '2020-07-21 08:22:46'),
-(22, 2, 50, 'Referral Amt from phpvishal@gmail.com', 'in', '', '2020-07-23 09:12:28');
+(25, 7, 2000, 'Republic Day Bonus', 'in', '', '2024-01-30 09:35:49');
 
 --
 -- Indexes for dumped tables
@@ -553,7 +524,7 @@ ALTER TABLE `banner`
 -- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `contact_us`
@@ -583,7 +554,7 @@ ALTER TABLE `dish`
 -- AUTO_INCREMENT for table `dish_cart`
 --
 ALTER TABLE `dish_cart`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `dish_details`
@@ -595,13 +566,13 @@ ALTER TABLE `dish_details`
 -- AUTO_INCREMENT for table `order_detail`
 --
 ALTER TABLE `order_detail`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `order_master`
 --
 ALTER TABLE `order_master`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `order_status`
@@ -625,13 +596,13 @@ ALTER TABLE `setting`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `wallet`
 --
 ALTER TABLE `wallet`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
